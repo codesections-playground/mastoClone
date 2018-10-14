63076fc91274abd3b10db2cca64f695abb5e1e0f
Daniel Sockwell <dsockwell@gmail.com>
2018-10-13 23:27:39 -0400

    Use create-react-app

---
+++ src/client/App.js
@@ -0,0 +1,20 @@
+// Using React, here are the components we'll use to mock Pinafore/Brutaldon
+
+/*
+NavBar - component to hold our navButtons
+  navButtons ie. notifications, community, local, search, etc...
+Timeline - component that holds the toots
+  a post toot component - (toot header, body, footer)
+  individual Toot components
+ */
+import React, { Component } from 'react';
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+export default App;
