a74882eb7e18ca772392c5d0cd4cb7666ea1d93c
Daniel Sockwell <dsockwell@gmail.com>
2018-10-13 21:30:48 -0400

    Initial structure

---
+++ client/app.js
@@ -0,0 +1,9 @@
+//Using React, here are the components we'll use to mock Pinafore/Brutaldon
+
+/*
+NavBar - component to hold our navButtons
+  navButtons ie. notifications, community, local, search, etc...
+Timeline - component that holds the toots
+  a post toot component - (toot header, body, footer)
+  individual Toot components
+ */
---
+++ server/app.js
@@ -0,0 +1,17 @@
+//Build a backend server that returns data (REST API) using Node.JS/Express
+
+/*
+Here in App.js we'd handle our routes, this will also act as our controllers (coming from the navBar on the client-side)
+Setup a basic server
+A database, MariaDB
+Middleware
+Models
+
+We're in a LEAN-style methodology.
+Build client first (Build only one component from navBar at one time)
+Build server w/o database first
+
+Build another navBar component
+Build server w/ database and connect the 2+
+Authentication
+ */
